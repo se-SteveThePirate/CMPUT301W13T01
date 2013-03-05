5c4cba7a8293c9fdee5542d613ece68e5817b306
seckert <seckert@uc06.cs.ualberta.ca>
2013-03-05 15:27:06 -0700

    Fixed methods in Ingredients class

--- Recipe Book of Awesome/src/Models/Ingredient.java
+++ Recipe Book of Awesome/src/Models/Ingredient.java
@@ -3,4 +3,20 @@ package Models;
 public class Ingredient {
 	public String name;
 	public Integer amount;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAmount() {
+		return this.amount;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
 }
