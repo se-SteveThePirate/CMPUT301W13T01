bf52dd4ea31f6e05c377799368a425846b0ad87b
bilec <bilec@ualberta.ca>
2013-03-25 14:49:37 -0600

    committing what i have done of the database handler, going for supper break, brb

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/Helpers/IngredientSQLite.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/Helpers/IngredientSQLite.java
@@ -1,7 +1,33 @@
 package ca.dreamteam.newrecipebook.Helpers;
 
+import java.util.ArrayList;
+import java.util.List;
+import ca.dreamteam.newrecipebook.Models.IngredientSqlTable;
+
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 
 public class IngredientSQLite
 {
+    private SQLiteDatabase database;
+    private IngredientSqlTable dbHelper;
+    private String[] allColumns = { IngredientSqlTable.COLUMN_ID,
+        IngredientSqlTable.COLUMN_INGREDIENT };
+    
+    public IngredientSQLite(Context context) {
+        dbHelper = new IngredientSqlTable(context);
+      }
+    
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+      }
 
+    public void close() {
+        dbHelper.close();
+      }
 }
