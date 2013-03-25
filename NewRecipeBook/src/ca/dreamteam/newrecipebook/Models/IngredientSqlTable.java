7c30081b294ddf4e9a8f928ce8ea3927dce60178
bilec <bilec@ualberta.ca>
2013-03-25 14:36:58 -0600

    fixed the SQL table class for ingredients

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/Models/IngredientSqlTable.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/Models/IngredientSqlTable.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-public class IngredientSqlTable
+public class IngredientSqlTable extends SQLiteOpenHelper
 {
     public static final String TABLE_INGREDIENTS = "ingredients";
     public static final String COLUMN_ID = "_id";
@@ -20,8 +20,8 @@ public class IngredientSqlTable
             + TABLE_INGREDIENTS + "(" + COLUMN_ID
             + " integer primary key autoincrement, " + COLUMN_INGREDIENT
             + " text not null);";
-/*
-    public MySQLiteHelper(Context context) {
+
+    public IngredientSqlTable(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
       }
 
@@ -33,12 +33,11 @@ public class IngredientSqlTable
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-      Log.w(MySQLiteHelper.class.getName(),
+      Log.w(IngredientSqlTable.class.getName(),
           "Upgrading database from version " + oldVersion + " to "
               + newVersion + ", which will destroy all old data");
-      db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+      db.execSQL("DROP TABLE IF EXISTS " + TABLE_INGREDIENTS);
       onCreate(db);
     }
-*/
-    //need to build our equivalent of SQLhelper before I can implement this code
+   
 }
