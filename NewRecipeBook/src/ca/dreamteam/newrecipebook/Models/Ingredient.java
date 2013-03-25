36811b2ed0b0584fc0b1f20d79344a884ba53b5e
bilec <bilec@ualberta.ca>
2013-03-25 14:15:55 -0600

    built and edited ingredients class in the new recipe book, BTW I am working with the new recipe book now

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/Models/Ingredient.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/Models/Ingredient.java
@@ -3,5 +3,22 @@ package ca.dreamteam.newrecipebook.Models;
 
 public class Ingredient
 {
-
+    private String id;
+    private String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getId(){
+        return id;
+    }
 }
