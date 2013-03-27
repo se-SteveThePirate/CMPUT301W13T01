f555488732cf46e9cc4597dce8d0d0452f4a6b32
seckert <seckert@winfield.cs.ualberta.ca>
2013-03-27 00:14:30 -0600

    Created RecipeListAdapter class in helpers to help displaying recipes

--- Recipe Book of Awesome/src/ca/dreamteam/recipebook/Models/Recipe.java
+++ Recipe Book of Awesome/src/ca/dreamteam/recipebook/Models/Recipe.java
@@ -11,15 +11,11 @@ public class Recipe {
 	public ArrayList<Bitmap> photos;
 	public ArrayList<Ingredient> ingredients;
 	public String instructions;
-<<<<<<< HEAD
 	public boolean starred;
-=======
 	public ArrayList<String> tags;
 	public Boolean globalEdit;
 	public ArrayList<String> comments;
 	public Boolean favorited;
-	
->>>>>>> 6117ed43793cf1d1ad453f7f0484c5a575944846
 	
 	public void uploadPhoto(Bitmap newPhoto)
 	{
