ef7474f345ad5dda54e713ee9ea7771c4dc1d048
NotAFirstYear <djames1@ualberta.ca>
2013-03-27 19:10:06 -0600

    Modifying and Removing Pantry items works now.

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/Helpers/IngredientDatabaseHelper.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/Helpers/IngredientDatabaseHelper.java
@@ -10,15 +10,15 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import ca.dreamteam.newrecipebook.Models.Ingredient;
 
-public class IngredientSQLite
+public class IngredientDatabaseHelper
 {
     private SQLiteDatabase database;
-    private IngredientSqlTable dbHelper;
-    private String[] allColumns = { IngredientSqlTable.COLUMN_ID,
-        IngredientSqlTable.COLUMN_INGREDIENT };
+    private IngredientSqlHelper dbHelper;
+    private String[] allColumns = { IngredientSqlHelper.COLUMN_ID,
+        IngredientSqlHelper.COLUMN_INGREDIENT };
     
-    public IngredientSQLite(Context context) {
-        dbHelper = new IngredientSqlTable(context);
+    public IngredientDatabaseHelper(Context context) {
+        dbHelper = new IngredientSqlHelper(context);
         
       }
     
@@ -41,10 +41,10 @@ public class IngredientSQLite
     		this.open();
     	}
     	ContentValues values = new ContentValues();
-        values.put(IngredientSqlTable.COLUMN_INGREDIENT, ingredient);
-        long insertId = database.insert(IngredientSqlTable.TABLE_INGREDIENTS, null, values);
-        Cursor cursor = database.query(IngredientSqlTable.TABLE_INGREDIENTS,
-                allColumns, IngredientSqlTable.COLUMN_ID + " = " + insertId, null,
+        values.put(IngredientSqlHelper.COLUMN_INGREDIENT, ingredient);
+        long insertId = database.insert(IngredientSqlHelper.TABLE_INGREDIENTS, null, values);
+        Cursor cursor = database.query(IngredientSqlHelper.TABLE_INGREDIENTS,
+                allColumns, IngredientSqlHelper.COLUMN_ID + " = " + insertId, null,
                 null, null, null);
         cursor.moveToFirst();
         Ingredient newIngredient = cursorToIngredient(cursor);
@@ -55,13 +55,12 @@ public class IngredientSQLite
     public void deleteIngredient(Ingredient ingredient) {
         long id = ingredient.getId();
         System.out.println("Ingredient deleted with id: " + id);
-        database.delete(IngredientSqlTable.TABLE_INGREDIENTS, IngredientSqlTable.COLUMN_ID
-            + " = " + id, null);
+        database.delete(IngredientSqlHelper.TABLE_INGREDIENTS, IngredientSqlHelper.COLUMN_ID + " = " + id, null);
       }
     
     public List<Ingredient> getAllIngredients(){
         List<Ingredient> ingredients = new ArrayList<Ingredient>();
-        Cursor cursor = database.query(IngredientSqlTable.TABLE_INGREDIENTS,
+        Cursor cursor = database.query(IngredientSqlHelper.TABLE_INGREDIENTS,
                 allColumns, null, null, null, null, null);
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
@@ -72,6 +71,15 @@ public class IngredientSQLite
         // Make sure to close the cursor
         cursor.close();
         return ingredients;
+    }
+    
+    public void updateIngredient(Ingredient ingredient)
+    {
+    	long id = ingredient.getId();
+    	ContentValues cv = new ContentValues();
+    	cv.put(IngredientSqlHelper.COLUMN_INGREDIENT, ingredient.getName());
+    	database.update(IngredientSqlHelper.TABLE_INGREDIENTS, cv, 
+    			IngredientSqlHelper.COLUMN_ID + "=" + id, null);
     }
 }
 
--- NewRecipeBook/src/ca/dreamteam/newrecipebook/Helpers/IngredientSqlHelper.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/Helpers/IngredientSqlHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-public class IngredientSqlTable extends SQLiteOpenHelper
+public class IngredientSqlHelper extends SQLiteOpenHelper
 {
     public static final String TABLE_INGREDIENTS = "ingredients";
     public static final String COLUMN_ID = "_id";
@@ -22,7 +22,7 @@ public class IngredientSqlTable extends SQLiteOpenHelper
             + " text not null);";
       
 
-    public IngredientSqlTable(Context context) {
+    public IngredientSqlHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
       }
 
@@ -34,7 +34,7 @@ public class IngredientSqlTable extends SQLiteOpenHelper
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-      Log.w(IngredientSqlTable.class.getName(),
+      Log.w(IngredientSqlHelper.class.getName(),
           "Upgrading database from version " + oldVersion + " to "
               + newVersion + ", which will destroy all old data");
       db.execSQL("DROP TABLE IF EXISTS " + TABLE_INGREDIENTS);
