b4a0679bc4fe169dc589073ae8feac9084ca9dc4
seckert <seckert@winfield.cs.ualberta.ca>
2013-03-27 19:34:24 -0600

    Worked on createrecipeactivity layout.

--- NewRecipeBook/gen/ca/dreamteam/newrecipebook/R.java
+++ NewRecipeBook/gen/ca/dreamteam/newrecipebook/R.java
@@ -15,12 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070015;
+        public static final int button1=0x7f07000b;
         public static final int button2=0x7f070019;
         public static final int button3=0x7f07001a;
         public static final int button4=0x7f070018;
-        public static final int editText1=0x7f070010;
-        public static final int editText2=0x7f070012;
+        public static final int editText1=0x7f070011;
+        public static final int editText2=0x7f070013;
         public static final int editText3=0x7f070017;
         public static final int imageView1=0x7f070021;
         public static final int ingredientAdd_multiPurposeButton=0x7f07000a;
@@ -29,10 +29,10 @@ public final class R {
         public static final int ingredientAdd_quantityEdit=0x7f070008;
         public static final int ingredientAdd_quantityLabel=0x7f070009;
         public static final int keywordSearchButton=0x7f070020;
-        public static final int mainActivity_MyPantry=0x7f07000b;
-        public static final int mainActivity_MySavedRecipes=0x7f07000c;
-        public static final int mainActivity_SearchRecipes=0x7f07000d;
-        public static final int mainActivity_TitleTextView=0x7f07000e;
+        public static final int mainActivity_MyPantry=0x7f07000c;
+        public static final int mainActivity_MySavedRecipes=0x7f07000d;
+        public static final int mainActivity_SearchRecipes=0x7f07000e;
+        public static final int mainActivity_TitleTextView=0x7f07000f;
         public static final int menu_settings=0x7f070022;
         public static final int pantrySearchButton=0x7f07001f;
         public static final int recipeActivity_emptyListViewText=0x7f07001b;
@@ -45,10 +45,10 @@ public final class R {
         public static final int recipeView_ingredientsLabel=0x7f070002;
         public static final int recipeView_ingredientsString=0x7f070005;
         public static final int recipeView_shareButton=0x7f070004;
-        public static final int textView1=0x7f07000f;
-        public static final int textView2=0x7f070011;
-        public static final int textView3=0x7f070013;
-        public static final int textView4=0x7f070014;
+        public static final int textView1=0x7f070010;
+        public static final int textView2=0x7f070012;
+        public static final int textView3=0x7f070014;
+        public static final int textView4=0x7f070015;
         public static final int textView5=0x7f070016;
     }
     public static final class layout {
