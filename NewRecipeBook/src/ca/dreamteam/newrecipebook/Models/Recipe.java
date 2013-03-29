dcc2c9b7aee65620c45b6bde749c3131df8424a2
David James <djames1@ualberta.ca>
2013-03-29 16:23:02 -0600

    Added serialVersionUID to Recipe class.

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/Models/Recipe.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/Models/Recipe.java
@@ -3,79 +3,75 @@ package ca.dreamteam.newrecipebook.Models;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-import ca.dreamteam.newrecipebook.Models.Ingredient;
-
-import android.content.Intent;
 import android.graphics.Bitmap;
 
 public class Recipe implements Serializable {
-    
-    
-    public long id;
-    public String name;
-    public String author;
-    public ArrayList<Bitmap> photos;
-    public ArrayList<String> ingredients;
-    public String instructions;
-    public ArrayList<String> tags;
-    public Boolean globalEdit;
-    public ArrayList<String> comments;
-    public Boolean favorited;
+	private static final long serialVersionUID = -3419759999296394802L;
+	public long id;
+	public String name;
+	public String author;
+	public ArrayList<Bitmap> photos;
+	public ArrayList<String> ingredients;
+	public String instructions;
+	public ArrayList<String> tags;
+	public Boolean globalEdit;
+	public ArrayList<String> comments;
+	public Boolean favorited;
 
-    public void uploadPhoto(Bitmap newPhoto)
-    {
-        //TODO
-    }
-    
-    public void setName(String name){
-        this.name = name;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    
-    public void setAuthor(String author){
-        this.author = author;
-    }
-     
-    public String getAuthor(){
-        return author;
-    }
-    
-    public ArrayList<String> getIngredients(){
-        return ingredients;
-    }
-    
-    public void setIngredients (ArrayList<String> ingredients){
-        this.ingredients = ingredients;
-    }
-    
-    
-    public void setId(long id){
-        this.id = id;
-    }
+	public void uploadPhoto(Bitmap newPhoto)
+	{
+		//TODO
+	}
 
-    public long getId(){
-        return id;
-    }
+	public void setName(String name){
+		this.name = name;
+	}
 
-    @Override
-    public String toString()
-    {
-    	return "";
-    }
-    
-    public String toEmailString()
-    {
-        return "<b>This needs to be formatted :)</b>";
-    }
-    
-    public void addIngredient(String ingredient) {
-    	ingredients.add(ingredient); 	
-    }
-    
-    public void addInstructions(String instructions) {
-    	this.instructions = instructions;
-    }
+	public String getName(){
+		return name;
+	}
+
+	public void setAuthor(String author){
+		this.author = author;
+	}
+
+	public String getAuthor(){
+		return author;
+	}
+
+	public ArrayList<String> getIngredients(){
+		return ingredients;
+	}
+
+	public void setIngredients (ArrayList<String> ingredients){
+		this.ingredients = ingredients;
+	}
+
+
+	public void setId(long id){
+		this.id = id;
+	}
+
+	public long getId(){
+		return id;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "";
+	}
+
+	public String toEmailString()
+	{
+		return "<b>This needs to be formatted :)</b>";
+	}
+
+	public void addIngredient(String ingredient) {
+		ingredients.add(ingredient); 	
+	}
+
+	public void addInstructions(String instructions) {
+		this.instructions = instructions;
+	}
 }
