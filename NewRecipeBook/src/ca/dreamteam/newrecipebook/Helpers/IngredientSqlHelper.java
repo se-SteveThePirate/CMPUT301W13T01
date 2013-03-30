e0236e0c73a5f13b760d1c64464eb0f414cce996
bilec <bilec@ualberta.ca>
2013-03-30 15:04:23 -0600

    added comments for the code in Ingrdient class and the two SQL ingredient classes

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/Helpers/IngredientSqlHelper.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/Helpers/IngredientSqlHelper.java
@@ -7,6 +7,8 @@ import android.util.Log;
 
 public class IngredientSqlHelper extends SQLiteOpenHelper
 {
+    //These variables are for the sql table that is created for storing ingredients
+    
     public static final String TABLE_INGREDIENTS = "ingredients";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_INGREDIENT = "ingredient";
@@ -15,22 +17,24 @@ public class IngredientSqlHelper extends SQLiteOpenHelper
     private static final String DATABASE_NAME = "ingredients.db";
     private static final int DATABASE_VERSION = 1;
     
+    //This statement formats and SQL query to get the actual SQL table
     private static final String DATABASE_CREATE = "create table "
             + TABLE_INGREDIENTS + "(" + COLUMN_ID
             + " integer primary key autoincrement, " + COLUMN_INGREDIENT
             + " text not null);";
       
-
+    //Gets the proper context of the table and database
     public IngredientSqlHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
       }
 
-    
+    //This method creates the database for ingredients
     @Override
     public void onCreate(SQLiteDatabase database) {
       database.execSQL(DATABASE_CREATE);
     }
 
+    //This method updates the database if there are any changes
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
       Log.w(IngredientSqlHelper.class.getName(),
