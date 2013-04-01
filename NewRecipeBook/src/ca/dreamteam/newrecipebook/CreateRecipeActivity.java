98546cd5bcb2f5bd98e399b2c51537798646a5ff
bilec <bilec@ualberta.ca>
2013-04-01 17:19:12 -0600

    merging files

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/CreateRecipeActivity.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/CreateRecipeActivity.java
@@ -48,7 +48,7 @@ public class CreateRecipeActivity extends Activity {
         setContentView(R.layout.activity_create_recipe);
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        newRecipe = (Recipe)getIntent().getSerializableExtra("recipe");
+        newRecipe = (Recipe)getIntent().getSerializableExtra("newRecipe");
         
         if (newRecipe == null)
         {
@@ -77,7 +77,7 @@ public class CreateRecipeActivity extends Activity {
         });
             }
         else {
-            this.newRecipe = (Recipe)getIntent().getSerializableExtra("recipe");
+            this.newRecipe = (Recipe)getIntent().getSerializableExtra("newRecipe");
             ((EditText)findViewById(R.id.recipeName)).setText(this.newRecipe.getName());
         }
 
