ff0ff1e045672c17856d9e780caaca52c37e780d
Maciej Ogrocki <ogrocki@cs.ualberta.ca>
2013-04-01 17:09:58 -0600

    Changed all the Connor's to Conner v3

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/CreateRecipeActivity.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/CreateRecipeActivity.java
@@ -29,7 +29,7 @@ import ca.dreamteam.newrecipebook.Models.Recipe;
  * then uploads them to the database
  * 
  * @version RecipeBook Project 4
- * @author Connor Bilec, David James, Steve Eckert and Maciej Ogrocki
+ * @author Conner Bilec, David James, Steve Eckert and Maciej Ogrocki
  * @date Monday 01 April 2013
  */
 
