4d9c45840b007e4f02224e1b609bafab480088e4
Maciej Ogrocki <ogrocki@cs.ualberta.ca>
2013-04-01 23:36:57 -0600

    Added documentation that was lost before

--- NewRecipeBook/src/ca/dreamteam/newrecipebook/SearchOptions.java
+++ NewRecipeBook/src/ca/dreamteam/newrecipebook/SearchOptions.java
@@ -5,14 +5,32 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.CheckBox;
+/**
+ * Searches the data by title author tags and ingredients or any combination of them. Uses check boxes to
+ * determine what to search by.
+ * 
+ * @version RecipeBook Project 4
+ * @author Conner Bilec, David James, Steve Eckert and Maciej Ogrocki
+ * @date Monday 01 April 2013
+ */
 
 public class SearchOptions extends Activity {
-	
+	/**
+	 * @var titleBox: If checked will search by names of recipes.
+	 * @var authorBox: If checked will search by users
+	 * @var tagBox: If checked will search by tags placed on recipes by users
+	 * @var ingredientBox: If checked will search by ingredient in the recipe. 
+	 */
 	private CheckBox titleBox;
 	private CheckBox authorBox;
 	private CheckBox tagBox;
 	private CheckBox ingredientsBox;
 
+	/**
+	 * As the file is created it checks which box's are check and updates the variables accordingly 
+	 * 
+	 * @param savedInstanceState
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +43,23 @@ public class SearchOptions extends Activity {
         
     }
 
+    /**
+     * returns true
+     * 
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //getMenuInflater().inflate(R.menu.activity_search_options, menu);
         return true;
     }
-    
+    /** 
+     * Checks which box's are checked and then searches for information accordingly. 
+     * Reorders list with those files which are more relevant. 
+     * 
+     * @param v
+     */
     public void search(View v) {
     	if (titleBox.isChecked()) {
     		//code for searching recipe titles
